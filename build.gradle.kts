a10d481fddaba960571a5358646759add5f97eb1
Ivan Pavlenko <mlg228glm@gmail.com.com>
2026-05-03 15:37:44 +0200

    updating dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,8 @@ plugins {
 
 dependencies {
     testImplementation("junit:junit:4.13.2")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.google.code.gson:gson:2.10.1")
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
         intellijIdea("2025.2.6.1")
